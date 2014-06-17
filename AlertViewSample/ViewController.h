75b6d3f00c105f9c61642b23cfd15fad7af9da8b
Ramdhas <ramdhasios@gmail.com>
2014-06-17 15:10:13 +0530

    AlertView_Sample_Ram
    Alertview will display without Button like ok or cancel, and alert will
    automatically hide after 2 seconds.

--- AlertViewSample/ViewController.h
+++ AlertViewSample/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    UIAlertView * alert;
+}
+- (IBAction)ShowAlert:(id)sender;
 @end
--- AlertViewSample/ViewController.m
+++ AlertViewSample/ViewController.m
@@ -26,4 +26,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ShowAlert:(id)sender
+{
+    alert = [[UIAlertView alloc]initWithTitle:@"Alert Displayed Successfully." message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:nil, nil];
+    [alert show];
+    
+    [self performSelector:@selector(DismissAlertViewAction:) withObject:alert afterDelay:2];
+    
+}
+-(void)DismissAlertViewAction:(UIAlertView *)alertView
+{
+    [alertView dismissWithClickedButtonIndex:0 animated:YES];
+}
 @end
